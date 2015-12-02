7fb95b07c45d911bf330051d31c98d429e576adb
Aaron <J>
2015-12-01 19:38:37 -0800

    Code cleanup.

--- MapquestClient.swift
+++ MapquestClient.swift
@@ -58,13 +58,13 @@ class MapquestClient: NSObject
     /// any shows that don't have lat/long info will get geocoded by Mapquest
     func geocodeShows(shows: [PhishShow], withType type: Services.Geocoding.GeocodingType, completionHandler: (geocodingError: NSError!) -> Void)
     {
-        print("Need to geocode \(shows.count) shows.")
         /// construct the request URL, starting with the base
         var mapquestRequestString = mapquestBaseURL + Services.Geocoding.GeocodingURL + Versions.Version1 + type.rawValue
         mapquestRequestString += "key=\(apiKey)"
         mapquestRequestString += "&maxResults=\(Options.MaxResults)&thumbMaps=\(Options.MapThumbnails)"
         
-        /// add each city to the batch geocoding request string
+        /// add each city to the batch geocoding request string;
+        /// record the shows with the same location, for use after the request is made
         var showsWithSameLocation = [String : Int]()
         var currentShowsWithSameLocation: Int = 1
         var previousCity: String = ""
@@ -74,6 +74,7 @@ class MapquestClient: NSObject
             /// don't geocode the same location more than once
             if show.city != previousCity
             {
+                /// update the dictionary and reset the counter
                 showsWithSameLocation.updateValue(currentShowsWithSameLocation, forKey: previousFixedCity)
                 currentShowsWithSameLocation = 1
                 
@@ -82,9 +83,11 @@ class MapquestClient: NSObject
                 city = city.stringByReplacingOccurrencesOfString(" ", withString: "")
                 mapquestRequestString += "&location=\(city)"
                 
+                /// remember the previous location
                 previousCity = show.city
                 previousFixedCity = city
                 
+                /// update the dictionary if we're at the last show
                 if index == shows.count - 1
                 {
                     showsWithSameLocation.updateValue(currentShowsWithSameLocation, forKey: previousFixedCity)
@@ -92,16 +95,16 @@ class MapquestClient: NSObject
             }
             else
             {
+                /// more than one show at the same location
                 ++currentShowsWithSameLocation
                 
+                /// update the dictionary if we're at the last show
                 if index == shows.count - 1
                 {
                     showsWithSameLocation.updateValue(currentShowsWithSameLocation, forKey: previousFixedCity)
                 }
             }
         }
-        
-        print("mapquestRequestString: \(mapquestRequestString)")
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0))
         {
@@ -134,11 +137,8 @@ class MapquestClient: NSObject
                             return
                         }
                         
-                        print("Got \(geocodeResults.count) geocoding results.")
-                        
                         /// extract the latitude/longitude coordinates for each location and set the values on each PhishShow object
                         /// update the progress bar on the tour map when each location is geocoded
-                        // var counter: Int = 0
                         let progressBump: Float = 0.2 / Float(shows.count)
                         var totalProgress: Float = self.tourMapProgressBar.progress
                         var nextShow: Int = 0
@@ -155,21 +155,23 @@ class MapquestClient: NSObject
                             let geocodedLatitude = latLong["lat"]!
                             let geocodedLongitude = latLong["lng"]!
                             
+                            /// get the number of shows with the same lat/long, from that dictionary from earlier
                             let providedLocation = result["providedLocation"] as! [String : AnyObject]
                             let city = providedLocation["location"] as! String
                             let numberOfShows: Int = showsWithSameLocation[city]!
                             
+                            /// set the latitude/longitude on the shows
                             for index in nextShow..<(nextShow + numberOfShows)
                             {
-                                /// set the latitude/longitude on the show
                                 shows[index].showLatitude = geocodedLatitude
                                 shows[index].showLongitude = geocodedLongitude
-                                print("Set the lat/long on shows[\(index)]")
                             }
+                            
+                            /// set the next index
                             nextShow += numberOfShows
                             
                             /// update the progress bar
-                            totalProgress += progressBump
+                            totalProgress += (progressBump * Float(numberOfShows))
                             dispatch_async(dispatch_get_main_queue())
                             {
                                 self.tourMapProgressBar.setProgress(totalProgress, animated: true)
@@ -190,188 +192,5 @@ class MapquestClient: NSObject
             }
             mapquestGeocodeRequest.resume()
         }
-    }
-    
-    /// take every location on the tour and geocode it to a latitude/longitude
-    func geocodeShowsForTour(tour: PhishTour, withType type: Services.Geocoding.GeocodingType, completionHandler: (geocodingError: NSError!) -> Void)
-    {
-        /// construct the request URL, starting with the base
-        var mapquestRequestString = mapquestBaseURL + Services.Geocoding.GeocodingURL + Versions.Version1 + type.rawValue
-        mapquestRequestString += "key=\(apiKey)"
-        mapquestRequestString += "&maxResults=\(Options.MaxResults)&thumbMaps=\(Options.MapThumbnails)"
-        
-        /// first, check to see if the locations need to be geocoded, and return if they already have been;
-        /// only geocode every unique location, not every show (some locations have multi-night runs)
-        if let uniqueLocations = tour.uniqueLocations
-        {
-            var counter: Int = 0
-            for location in uniqueLocations
-            {
-                if location.city == ""
-                {
-                    continue
-                }
-                
-                /// check the coordinates
-                if location.showLatitude != 0 && location.showLongitude != 0
-                {
-                    /// we checked all the locations
-                    if counter == uniqueLocations.count - 1
-                    {
-                        /// complete with no error
-                        completionHandler(geocodingError: nil)
-                        return
-                    }
-                    else
-                    {
-                        /// check next location
-                        counter++
-                        continue
-                    }
-                }
-                /// there's a location that needs to be geocoded
-                else
-                {
-                    /// turn the location into a string that can be appended to the request;
-                    /// some locations need additional formatting
-                    var city = location.city
-                    city = city.stringByReplacingOccurrencesOfString(" ", withString: "")
-                    city = self.fixSpecialCities(city)
-                    mapquestRequestString += "&location=\(city)"
-                    counter++
-                }
-            }
-        }
-        else
-        {
-            /// create an error to send back
-            var geocodingErrorDictionary = [NSObject : AnyObject]()
-            geocodingErrorDictionary.updateValue("Couldn't get the tour locations.", forKey: NSLocalizedDescriptionKey)
-            let geocodingError = NSError(domain: NSCocoaErrorDomain, code: 9999, userInfo: geocodingErrorDictionary)
-            completionHandler(geocodingError: geocodingError)
-        }
-        
-        /// run the request on a background thread, update the progress bar on the main thread
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0))
-        {
-            dispatch_async(dispatch_get_main_queue())
-            {
-                self.tourMapProgressBar.setProgress(0.8, animated: true)
-            }
-            
-            /// create the URL and start the task
-            let mapquestRequestURL = NSURL(string: mapquestRequestString)!
-            let mapquestGeocodeRequest = self.session.dataTaskWithURL(mapquestRequestURL)
-            {
-                mapquestData, mapquestResponse, mapquestError in
-                
-                /// there was an error geocoding the locations
-                if mapquestError != nil
-                {
-                    completionHandler(geocodingError: mapquestError)
-                }
-                else
-                {                    
-                    do
-                    {
-                        let jsonMapquestData = try NSJSONSerialization.JSONObjectWithData(mapquestData!, options: []) as! [String : AnyObject]
-                        guard let geocodeResults = jsonMapquestData["results"] as? [[String : AnyObject]]
-                        else
-                        {
-                            completionHandler(geocodingError: mapquestError!)
-                            return
-                        }
-                        
-                        /// extract the latitude/longitude coordinates for each location and set the values on each PhishShow object
-                        /// update the progress bar on the tour map when each location is geocoded
-                        var counter: Int = 0
-                        let progressBump: Float = 0.2 / Float(tour.uniqueLocations!.count)
-                        var totalProgress: Float = self.tourMapProgressBar.progress
-                        for result in geocodeResults
-                        {
-                            /// get the current show, see if its part of a multi-night run, then get all the shows associated with the location
-                            guard let currentShow: PhishShow = tour.shows[counter]
-                            else
-                            {
-                                completionHandler(geocodingError: mapquestError!)
-                                return
-                            }
-                            guard let shows: [PhishShow] = Array(tour.shows[counter...(counter + (Int(currentShow.consecutiveNights) - 1))])
-                            else
-                            {
-                                completionHandler(geocodingError: mapquestError!)
-                                return
-                            }
-                            
-                            /// get at the latitude/longitude info
-                            let locations = result["locations"] as! [AnyObject]
-                            guard let innerLocations = locations[0] as? [String : AnyObject]
-                            else
-                            {
-                                continue
-                            }
-                            let latLong = innerLocations["latLng"] as! [String : Double]
-                            let geocodedLatitude = latLong["lat"]!
-                            let geocodedLongitude = latLong["lng"]!
-                            
-                            /// set the latitude/longitude on each show associated with the multi-night run
-                            /// (or just on the one show, if there weren't consecutive nights)
-                            for show in shows
-                            {
-                                show.showLatitude = geocodedLatitude
-                                show.showLongitude = geocodedLongitude
-                            }
-                            
-                            /// jump to the first show at the next location
-                            counter += Int(currentShow.consecutiveNights)
-                            
-                            /// update the progress bar
-                            totalProgress += progressBump
-                            dispatch_async(dispatch_get_main_queue())
-                            {
-                                self.tourMapProgressBar.setProgress(totalProgress, animated: true)
-                            }
-                        }
-                        
-                        /// save new and updated objects to the context
-                        CoreDataStack.sharedInstance().saveContext()
-                        
-                        /// everything completed successfully
-                        completionHandler(geocodingError: nil)
-                    }
-                    catch
-                    {
-                        print("There was a problem parsing the geocoding data from mapquest.com")
-                    }
-                }
-            }
-            mapquestGeocodeRequest.resume()
-        }
-    }
-    
-    func fixSpecialCities(var location: String) -> String
-    {
-        switch location
-        {
-            case "Nüremberg,Germany":
-                location = "Nuremberg,Germany"
-                
-            case "Lyon/Villeurbanne,France":
-                location = "Lyon,France"
-                
-            case "Montréal,Québec,Canada":
-                location = "Montreal,Quebec,Canada"
-                
-            case "Düsseldorf,Germany":
-                location = "Dusseldorf,Germany"
-                
-            case "OrangeBeach,ALUS":
-                location = "OrangeBeach,AL"
-                
-            default:
-                break
-        }
-        
-        return location
     }
 }
